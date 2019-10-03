c090fba64922e7900a124279d4a7bbbaf574a6d1
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2019-10-03 06:10:52 -0700

    Adding IFD Template

---
+++ includes/templates/IFD/template_info.php
@@ -0,0 +1,6 @@
+<?php
+$template_name = 'Indianapolis Fire Department';
+$template_version = 'Version 1.0';
+$template_author = 'Anne, www.picaflor-azul.com (c) 2015';
+$template_description = 'This HTML5 template is mobile friendly and uses contemporary Responsive CSS to allow sharing of a single template across multiple device types.  It uses PHP Mobile Detect to serve up the optimized layout based on device. It allows the designer to implement device-specific php, jquery, javascript or ajax. This template was cloned from IEMS on 10/03/2019 09:09:20.';
+$template_screenshot = 'scr_responsive_classic.jpg';
