21eafc3bc399a919b8eb05b148ee128840cdc767
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2021-11-19 10:20:17 -0500

    WTFD mods for address and Zip

--- includes/functions/extra_functions/iems.php
+++ includes/functions/extra_functions/iems.php
@@ -21,4 +21,19 @@ function unit_lookup() {
 	}
 
 
+function unit_lookup_filter() {
+	global $db;
+	global $unit_array;
+	$filter = '49 IEMS';
+
+	$unit_array = array();
+	$unit_values = $db->Execute("select unit_description from `units` where unit_filter ='" .  $filter . "' order by unit_description");
+
+		while (!$unit_values->EOF) {
+			$unit_array[] = array('id' => $unit_values->fields['unit_description'], 'text' => 			$unit_values->fields['unit_description']);
+			$unit_values->MoveNext();
+			};
+	return $unit_array; 
+	}
+
 ?>
