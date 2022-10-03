e17429eb964cde113ebd824e19bc77019c68551c
Jeremiah Cook <jeremiah.cook@indianapolisems.org>
2022-10-03 05:39:30 -0700

    debugging tool added for minion/index.php

--- minion/index_dashboard.php
+++ minion/index_dashboard.php
@@ -12,6 +12,7 @@ $number_of_visitors_history = 15;
 
 $notifications = new AdminNotifications();
 $availableNotifications = $notifications->getNotifications('index', $_SESSION['admin_id']);
+print_r($_SESSION);
 
 require(DIR_WS_CLASSES . 'currencies.php');
 $currencies = new currencies();
